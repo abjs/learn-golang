8f1c658d13f2c135ecf1e371353377c09f1e64b4
Abin Joseph <itsmeabjs@gmail.com>
2022-03-27 13:16:03 +0530

    docs: variables and type conversion

--- main.go
+++ main.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	name := "Gopher"
+	age := 23
+	fmt.Printf("Hi i am %s and i am %d Yeses old\n", name, age)
+
+	var ageFloat float32 = float32(age)
+	fmt.Printf("Hi i am %s and i am %f Yeses old\n", name, ageFloat)
+
+	var ageString string = strconv.Itoa(age)
+	fmt.Printf("Hi i am %s and i am %s Yeses old\n", name, ageString)
 }
