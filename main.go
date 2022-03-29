1bb0cbc3bd42ebbe52cf0f10cf1999e91ee6f5c4
Abin Joseph <itsmeabjs@gmail.com>
2022-03-29 11:07:13 +0530

    docs: hello word at root rount

--- main.go
+++ main.go
@@ -4,6 +4,11 @@ import (
 	"net/http"
 )
 
+func helloWord(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello World"))
+}
+
 func main() {
+	http.HandleFunc("/", helloWord)
 	http.ListenAndServe(":8080", nil)
 }
