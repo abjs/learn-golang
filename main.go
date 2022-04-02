2d2b2adb6dda79ebf1192666b0aa04765b8feb03
Abin Joseph <itsmeabjs@gmail.com>
2022-04-02 18:40:22 +0530

    docs: package with dff files

--- main.go
+++ main.go
@@ -3,15 +3,6 @@ package main
 import "fmt"
 
 func main() {
-	const pi = 3.14
-	fmt.Printf("value is %v\t type is %T\n", pi, pi)
-	const a int = 10
-	const b float32 = 10.26
-	const c string = "hello"
-	const d bool = true
-	fmt.Printf("value is %v\t type is %T\n", a, a)
-	fmt.Printf("value is %v\t type is %T\n", b, b)
-	fmt.Printf("value is %v\t type is %T\n", c, c)
-	fmt.Printf("value is %v\t type is %T\n", d, d)
-
+	fmt.Print("user Name is :")
+	fmt.Println(userName)
 }
